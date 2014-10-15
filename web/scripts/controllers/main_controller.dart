736989cfc41cdd31f492c1c317e17a6404133d90
namiheike <namiheike@gmail.com>
2014-10-15 11:48:57 +0800

    refresh

--- web/scripts/controllers/main_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:fishbone/fishbone.dart' as fishbone;
-
-class MainController extends fishbone.Controller {
-  void home() {
-    super.render('home');
-  }
-}
-
-var mainController = new MainController();
--- web/scripts/views/main/new.js
+++
@@ -1,7 +0,0 @@
-function template(locals) {
-var buf = [];
-var jade_mixins = {};
-var jade_interp;
-;var locals_for_with = (locals || {});(function (v) {
-buf.push("<div class=\"new\"><new></new>" + (jade.escape(null == (jade_interp = v) ? "" : jade_interp)) + "</div>");}.call(this,"v" in locals_for_with?locals_for_with.v:typeof v!=="undefined"?v:undefined));;return buf.join("");
-}
